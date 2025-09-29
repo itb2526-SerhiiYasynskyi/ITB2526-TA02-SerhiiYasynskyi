abf1408c400c43a30d54df4b96136b66b27c4240
serhii.yasynskyi.7e9 <serhii.yasynskyi.7e9@itb.cat>
2025-09-29 11:47:45 +0200

    oooo

---
+++ HelloWorld.py
@@ -0,0 +1,22 @@
+"""
+
+Serhii Yasynskyi
+
+12/09/2023
+
+ASIXc M03 UF1 A1
+
+Descripció: Exemple dels apunts
+
+"""
+
+# Programa que demana l'edat i diu si ets major d'edat.
+
+edat = int(input("Quina edat tens? "))
+
+if edat >= 18:
+    print("Ets major d'edat")
+else:
+    print("Ets menor d'edat")
+
+print("Programa finalitzat")
